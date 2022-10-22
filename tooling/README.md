2ff4a80ff413972d0409bd593a8571388ccf56c2
Constantinos Georgiou <constantgeorgiou@gmail.com>
2022-10-22 19:25:02 +0300

    Complete TypeScript Tooling in 5 minutes

---
+++ tooling/README.md
@@ -0,0 +1,1 @@
+# [TypeScript Tooling in 5 minutes](https://www.typescriptlang.org/docs/handbook/typescript-tooling-in-5-minutes.html)
---
+++ tooling/greeter.ts
@@ -0,0 +1,18 @@
+// function greeter(person: string): string {
+//     return "Hello, " + person;
+// }
+
+// let user = "Jane User";
+
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
+
+// function greeter(person: Person) {
+//     return "Hello, " + person.firstName + " " + person.lastName;
+// }
+
+let user = { firstName: "Jane", lastName: "User" };
+
+document.body.textContent = greeter(user);
